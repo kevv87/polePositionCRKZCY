0ad29b0f085909a19544cf7ff757c0bd1d347785
Cvaz <scyock.03@gmail.com>
2020-06-08 22:46:26 -0600

    Fresh new start :)

--- polePositionGUI/src/GameObjects/Static.java
+++
@@ -1,8 +0,0 @@
-package GameObjects;
-
-// Clase designada a objetos estaticos: vidas, huecos, boosts.
-public class Static extends Drawable{
-
-    String type; // El tipo de objeto estatico: vida, hueco o boost.
-
-}
